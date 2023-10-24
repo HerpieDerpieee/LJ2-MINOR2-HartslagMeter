492edf41a7fa61ff41fc891d25829b48d39d519c
valentijn <herpes@derpes.com>
2023-10-24 20:40:00 +0200

    fixed some minor bugs with the trasition function

--- website/js/ingame.js
+++ website/js/ingame.js
@@ -3,33 +3,42 @@ window.addEventListener("keydown", checkKeyPressed, false);
 
 
 function checkKeyPressed(evt) {
-    if (evt.keyCode == "83" && started == false) {
+    console.log(evt.keyCode)
+    if (evt.keyCode.toString() === "83" && started === false) {
         start()
-    }else if (evt.keyCode == "83" && started == true){
+    } else if (evt.keyCode.toString() === "83" && started === true) {
         end()
     }
+}
 function start(){
+    started = true;
     document.getElementById('blackscreen').style.opacity = 1;
     setTimeout(() => {
         document.getElementById('tablebody').style.display = 'none';
     }, 2000);
+
     setTimeout(() => {
-    document.getElementById('ingamebody').style.display = 'flex';
+        document.getElementById('ingamebody').style.display = 'flex';
     }, 2000);
-        setTimeout(() => {
-    document.getElementById('blackscreen').style.opacity = 0;
-        }, 2000);
+
+    setTimeout(() => {
+        document.getElementById('blackscreen').style.opacity = 0;
+    }, 2000);
 }
 
 function end(){
+    started = false;
     document.getElementById('blackscreen').style.opacity = 1;
+
     setTimeout(() => {
         document.getElementById('ingamebody').style.display = 'none';
     }, 2000);
+
     setTimeout(() => {
-    document.getElementById('tablebody').style.display = 'flex';
+        document.getElementById('tablebody').style.display = 'flex';
     }, 2000);
-        setTimeout(() => {
-    document.getElementById('blackscreen').style.opacity = 0;
-        }, 2000);
+
+    setTimeout(() => {
+        document.getElementById('blackscreen').style.opacity = 0;
+    }, 2000);
 }
