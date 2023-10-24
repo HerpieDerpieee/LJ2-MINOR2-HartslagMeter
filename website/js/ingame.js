bb0b656ea7fc36b275f4a270540762f5dfca6aea
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-10-24 17:15:46 +0200

    Update ingame.js
    Hopelijk transision terug af

--- website/js/ingame.js
+++ website/js/ingame.js
@@ -1,10 +1,13 @@
+let started = false;
 window.addEventListener("keydown", checkKeyPressed, false);
 
+
 function checkKeyPressed(evt) {
-    if (evt.keyCode == "83") {
+    if (evt.keyCode == "83" && started == false) {
         start()
+    }else if (evt.keyCode == "83" && started == true){
+        end()
     }
-}
 function start(){
     document.getElementById('blackscreen').style.opacity = 1;
     setTimeout(() => {
@@ -12,6 +15,19 @@ function start(){
     }, 2000);
     setTimeout(() => {
     document.getElementById('ingamebody').style.display = 'flex';
+    }, 2000);
+        setTimeout(() => {
+    document.getElementById('blackscreen').style.opacity = 0;
+        }, 2000);
+}
+
+function end(){
+    document.getElementById('blackscreen').style.opacity = 1;
+    setTimeout(() => {
+        document.getElementById('ingamebody').style.display = 'none';
+    }, 2000);
+    setTimeout(() => {
+    document.getElementById('tablebody').style.display = 'flex';
     }, 2000);
         setTimeout(() => {
     document.getElementById('blackscreen').style.opacity = 0;
