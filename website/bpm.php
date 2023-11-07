d164c11d347cf45d99190a4e6102bd2f8d5020f4
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-11-07 16:23:37 +0100

    Update bpm.php

--- website/bpm.php
+++ website/bpm.php
@@ -1,5 +1,9 @@
 <?php
+include "db.php";
 
-$bpm  = rand(60,120);
+$SQLQ= "SELECT lastBPM, peakBPM FROM IOT_Scores ORDER BY 'id' DESC LIMIT 1";
+$result = $conn->query($SQLQ);
+$row = $result->fetch_assoc();
+$bpm  = $row["lastBPM"];
 
 echo $bpm." BPM";
