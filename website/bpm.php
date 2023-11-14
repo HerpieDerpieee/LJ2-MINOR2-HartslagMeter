240314f598d9a7f904aef448359d2ba7662450ac
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-11-14 14:24:55 +0100

    ja gekke verandering snapje

--- website/bpm.php
+++ website/bpm.php
@@ -1,9 +1,19 @@
 <?php
+
 include "db.php";
 
 $SQLQ= "SELECT * FROM `IOT_Scores` ORDER BY `IOT_Scores`.`id` DESC;";
 $result = $conn->query($SQLQ);
 $row = $result->fetch_assoc();
-$bpm  = $row["lastBPM"];
+$bpm = $row["lastBPM"];
+$highestbpm = $row["peakBPM"];
 
 echo $bpm." BPM";
+
+if ($highestbpm<$bpm){
+    $highestbpm = $bpm;
+    $SQLS= "UPDATE `IOT_Scores` SET `peakBPM`= ".$highestbpm." WHERE id=(SELECT MAX(id) FROM `IOT_Scores`);";
+    $sent = $conn->query($SQLS);
+}elseif ($highestbpm>$bpm){
+echo "<script> console.log(".$highestbpm.")</script>";
+}
