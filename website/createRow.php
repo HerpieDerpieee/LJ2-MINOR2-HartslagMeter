845aed7ab89cbfe1a446d648a39550a3ae3c04d6
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-11-16 11:57:04 +0100

    spel start en stop ):)

--- website/createRow.php
+++ website/createRow.php
@@ -8,14 +8,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Use prepared statements to prevent SQL injection
     $stmt = $conn->prepare("INSERT INTO IOT_Scores (startingTime, duration, username, lastBPM, peakBPM) VALUES (CURRENT_TIMESTAMP, 0, ?, 0, 0)");
     $stmt->bind_param("s", $username);
+    $stmand = $conn->prepare("UPDATE IOT_Running SET running = 1 ");
+
+   
+
 
     if ($stmt->execute()) {
         echo "New record created successfully: ".$username;
     } else {
         echo "Error: " . $stmt->error;
     }
+    if ($stmand->execute()) {
+        echo "New record created successfully: spel start :)";
+    } else {
+        echo "Error: " . $stmand->error;
+    }
 
     $stmt->close();
+    $stmand->close();
     $conn->close();
 } else {
     echo "Invalid request method";
--- website/endGame.php
+++ website/endGame.php
@@ -8,14 +8,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Use prepared statements to prevent SQL injection
     $stmt = $conn->prepare("UPDATE IOT_Scores SET duration=? WHERE id=(SELECT MAX(id) FROM IOT_Scores)");
     $stmt->bind_param("i", $duration);
+    $stmand = $conn->prepare("UPDATE IOT_Running SET running=0 ");
 
     if ($stmt->execute()) {
         echo "New record created successfully: ".$duration;
     } else {
         echo "Error: " . $stmt->error;
     }
+    if ($stmand->execute()) {
+        echo "New record created successfully: spel stop :(";
+    } else {
+        echo "Error: " . $stmand->error;
+    }
+
+
 
     $stmt->close();
+    $stmand->close();
     $conn->close();
 } else {
     echo "Invalid request method";
